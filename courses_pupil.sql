6ae7a2f483effebde7619c63d3346d6154cc76e2
sarthakbwj <156067427+sarthakbwj@users.noreply.github.com>
2024-10-16 10:54:50 +0530

    Update courses_pupil.sql

--- courses_pupil.sql
+++ courses_pupil.sql
@@ -11,7 +11,22 @@ VALUES
 (102, "bob"),
 (103, "casey");
 
+CREATE TABLE courses(
+ id INT PRIMARY KEY,
+ course VARCHAR(50)
+);
 
+INSERT INTO courses(id,course)
+VALUES 
+(102, "english"),
+(105, "math"),
+(103, "science"),
+(107, "computer science");
 
+-- INNER JOIN
+SELECT *
+FROM pupil
+INNER JOIN courses
+ON pupil.id = courses.id;
 
 

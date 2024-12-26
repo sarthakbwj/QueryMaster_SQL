c06177d8ba64e86d486c4a26936327293bde04c9
sarthakbwj <156067427+sarthakbwj@users.noreply.github.com>
2024-12-26 20:00:38 +0530

    keys

--- Lecture 3/schema.sql
+++ Lecture 3/schema.sql
@@ -1,12 +1,14 @@
 CREATE TABLE "riders" (
     "id" INTEGER,
-    "name" TEXT
+    "name" TEXT,
+    PRIMARY KEY ("id")
 ); 
 
 CREATE TABLE "stations" (
     "id" INTEGER,
     "name" TEXT, 
-    "line" TEXT
+    "line" TEXT,
+    PRIMARY KEY ("id")
 );
 
 CREATE TABLE "visits" (
